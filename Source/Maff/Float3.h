3b0eef3f56dd8cc6bc22ce52ab754d9734b8232c
CM\ruta.sapokaite <rutaspk@gmail.com>
2022-05-02 17:09:51 +0200

    float 3

---
+++ Source/Maff/Float3.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+USTRUCT()
+struct MAFF_API Float3
+{
+	float x, y, z;
+	Float3() = default;
+	~Float3() = default;
+	Float3(float x, float y, float z);
+
+	float& operator[](int i);
+	const float& operator[](int i) const;
+
+	Float3& operator *=(float s);
+	Float3& operator /=(float s);
+	Float3 operator *(float s) const;
+	Float3 operator /(float s) const;
+	
+	Float3 operator -(const Float3& v) const;
+	Float3& operator +=(const Float3& v);
+	Float3& operator -=(const Float3& v);
+	Float3 operator +(const Float3& v) const;
+	Float3 operator -(const Float3& v);
+	
+	
+	float Magnitude() const;
+	Float3 Normalize() const;
+};
